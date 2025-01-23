d1cb15b666f3bece55e6e5545a56a64ed980d27f
myName <my@email.com>
2025-01-23 14:32:48 +0300

    finish jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Book } from "./Book";
+
+const App = () => {
+    return (
+        <div>
+            <Book name="JS for beginners" year="2018" price="1000" />
+            <Book name="JS for beginners" year="2018" price="1000" />
+            <Book name="JS for beginners" year="2018" price="1000" />
+            <Book name="JS for beginners" year="2018" price="1000" />
+        </div>
+    );
+};
+
+export default App;
